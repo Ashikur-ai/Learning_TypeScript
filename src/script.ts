4c70132a5737d1578002ca863a77dba3a7bd290f
ashiku2 <ashikur1602@gmail.com>
2024-11-03 08:36:13 +0600

    learning class in typescript

--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,20 @@
-let a: any[] = [];
+class Player{
+  name: string;
+  age: number;
+  country: string;
 
-a.push("Bangladesh");
-a.push(43);
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
 
-// object 
-let b: {
-  name: any,
-  age: any
-};
-
-b = {
-  name: 'Bangladesh',
-  age: 34
+  play() {
+    console.log(`${this.name} from ${this.country} is playing`);
+  }
 }
 
+const mashrafi = new Player("Masrafi", 54, "Bangladesh");
+const sakib = new Player("Sakib", 35, "Bangladesh");
+
+const players: string[] = [];
